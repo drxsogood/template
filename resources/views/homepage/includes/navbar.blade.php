3e4fd2ef541bca75ef9403e4515a52ef3d9e4806
drxsogood <edrianeopalia09@gmail.com>
2023-01-15 22:38:32 +0800

    Adding Templates

---
+++ resources/views/homepage/includes/navbar.blade.php
@@ -0,0 +1,70 @@
+ <!-- Sub Header -->
+ <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-sm-8">
+          <div class="left-content">
+            <p>Check us on our <em>social media</em> platforms</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-4">
+          <div class="right-icons">
+            <ul>
+              <li><a href="https://www.facebook.com/boscoffee"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="https://twitter.com/boscoffee"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="https://www.linkedin.com/company/boscoffee/"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+      <div class="container">
+          
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="index.html" class="logo">
+                        Bo's Coffee
+                      </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav">
+                          <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                          <li class="scroll-to-section"><a href="#apply">About us</a></li>
+                          <li class="scroll-to-section"><a href="#courses">Menu</a></li> 
+                          <li class="scroll-to-section"><a href="#contact">Contact Us</a></li> 
+                      </ul>        
+                      <a class='menu-trigger'>
+                          
+                      </a>
+                      <!-- ***** Menu End ***** -->
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+  <!-- ***** Main Banner Area Start ***** -->
+  <section class="section main-banner" id="top" data-section="section1">
+      <video autoplay muted loop id="bg-video">
+          <source src="assets/images/coffee.mp4" type="video/mp4"/>
+      </video>
+      <div class="video-overlay header-text">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="caption">
+              <h6>Life is better with COFFEE</h6>
+              <h2>The Best Coffee Testing <em>Experience</em></h2>
+              <p>Coffee is more than a beverage, however. It is a memory, an anticipation, a lifetime of consoling moments of modest pleasure woven into our lives. Coffee Review is devoted to promoting the fine, distinctive cup and celebrating its lore and pleasures. <a rel="nofollow" href="https://www.boscoffee.com/" target="_blank">Bo's Coffee</a>.</p>
+          </div>
+              </div>
+            </div>
+          </div>
+      </div>
+  </section>
+  <!-- ***** Main Banner Area End ***** -->
